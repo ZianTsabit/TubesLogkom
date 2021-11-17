cac1b20401d6c01f4c473f8711d5ea8b59aaab26
Vincent136 <13520136@std.stei.itb.ac.id>
2021-11-17 20:36:36 +0700

    tambah rules gerak

--- src/main.pl
+++ src/main.pl
@@ -3,6 +3,12 @@
 
 :- include('map.pl').
 
+menu                    :- write('Welcome to <nama game>!'),nl ,nl ,
+                           write('Ketik 1 untuk mulai (belom dibikin opsi lain).'), nl,
+                           read_integer(Choice),
+                           (Choice = 1 -> write('Hello, Player, welcome to <nama game>')), nl, start, !.
 
-map :-                  map(0, 16), nl.
+:- dynamic(isStarted/1).
+start                   :- isStarted(_), write("you already start your journey!").
 
+start                   :- \+isStarted(_), asserta(started(true)), createMap.
--- src/map.pl
+++ src/map.pl
@@ -12,12 +12,17 @@ generate(X,Y) :-        (X = 0, Y = 0 -> asserta(coordinate(0, 0));
 		                asserta(coordinate(X, Y)), NewY is Y - 1, generate(X, NewY)).
 
 
-isWall(X, Y) :-         Y =:= 16, coordinate(X, Y).
-isWall(X, Y) :-         X =:= 0, coordinate(X, Y).
-isWall(X, Y) :-         X =:= 16, coordinate(X, Y).
-isWall(X, Y) :-         Y =:= 0, coordinate(X, Y).
+wall(X, Y) :-         Y =:= 16, coordinate(X, Y).
+wall(X, Y) :-         X =:= 0, coordinate(X, Y).
+wall(X, Y) :-         X =:= 16, coordinate(X, Y).
+wall(X, Y) :-         Y =:= 0, coordinate(X, Y).
 
-
+:- dynamic(player/2).
+:- dynamic(house/2).
+:- dynamic(ranch/2).
+:- dynamic(quest/2).
+:- dynamic(marketplace/2).
+:- dynamic(digged/2).
 
 createMap :-            generate(16,16),
                         asserta(player(4,10)),
@@ -37,14 +42,46 @@ createRiver :-          asserta(water(15,5)),
                         asserta(water(15,2)),asserta(water(14,2)),asserta(water(13,2)),asserta(water(12,2)),asserta(water(11,2)),
                         asserta(water(15,1)),asserta(water(14,1)),asserta(water(13,1)),asserta(water(12,1)),asserta(water(11,1)),asserta(water(10,1)).
 
-map(X, Y) :-          (isWall(X, Y) -> write('#');
+move(BeforeX, BeforeY, AfterX, AfterY) :-   wall(AfterX, AfterY) -> write('Oh No! the wall is too huge!'),
+                                            retract(player(AfterX,AfterY)), asserta(player(BeforeX, BeforeY)), nl, !;
+                                            water(AfterX, AfterY) -> write('You can not walk on the water, fellas...'), nl, !,
+                                            retract(player(AfterX,AfterY)), asserta(player(BeforeX, BeforeY));
+                                            (house(AfterX,AfterY) -> write('You are in your house!');
+                                            ranch(AfterX,AfterY) -> write('You are in your ranch!');
+                                            marketplace(AfterX,AfterY) -> write('You are in marketplace!');
+                                            digged(AfterX,AfterY) -> write('you are standing on digged tile!')).
+
+map :-                  map(0, 16), nl.
+
+map(X, Y) :-            (wall(X, Y) -> write('#');
                         player(X, Y) -> write('P');
                         house(X, Y) -> write('H');
                         ranch(X, Y) -> write('R');
                         quest(X,Y) -> write('Q');
                         marketplace(X, Y) -> write('M');
+                        digged(X,Y) -> write('=');
                         water(X,Y) -> write('o');
                         write('-')), X2 is X + 1, 
                         (X = 16, Y = 0 -> nl;
                         X = 16 -> nl, X1 = 0, Y1 is Y - 1, map(X1, Y1);
                         map(X2, Y)).
+
+w :-                    retract(player(BeforeX, BeforeY)),
+                        AfterY is BeforeY + 1, 
+                        asserta(player(BeforeX, AfterY)),
+                        move(BeforeX,BeforeY,BeforeX, AfterY).
+
+a :-                    retract(player(BeforeX, BeforeY)),
+                        AfterX is BeforeX - 1, 
+                        asserta(player(AfterX, BeforeY)),
+                        move(BeforeX,BeforeY,AfterX, BeforeY).
+
+s :-                    retract(player(BeforeX, BeforeY)),
+                        AfterY is BeforeY - 1, 
+                        asserta(player(BeforeX, AfterY)),
+                        move(BeforeX,BeforeY,BeforeX, AfterY).
+
+d :-                    retract(player(BeforeX, BeforeY)),
+                        AfterX is BeforeX + 1, 
+                        asserta(player(AfterX, BeforeY)),
+                        move(BeforeX,BeforeY,AfterX, BeforeY).
