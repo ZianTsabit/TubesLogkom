1bae6170243452dd04bb4ddfb6f4f8d3f9ef54cb
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-18 19:47:38 +0700

    add start

--- src/main.pl
+++ src/main.pl
@@ -43,11 +43,20 @@ help:-
     write('% 8. help   : menampilkan segala bantuan                                       %'),nl,
     write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%').
 
+:- dynamic(job/1).
 :- dynamic(isStarted/1).
+:- dynamic(isQuest/1).
 start                   :- isStarted(_) -> write('you already start your journey!'), !.
 
-start                   :- \+isStarted(_), asserta(isStarted(true)), createMap.
+start                   :- \+isStarted(_), asserta(isStarted(true)), createMap, 
+                            write('Welcome to Harvest Star. Choose your job'), nl,
+                            write('1. Fisherman'), nl,
+                            write('2. Farmer'), nl,
+                            write('3. Rancher'), nl,
+                            write('> '), read_integer(Choice),
+                            (Choice = 1 -> write('You choose Fisherman, lets start fishing'), asserta(job(fisherman))),
+                            (Choice = 2 -> write('You choose Farmer, lets start farming'), asserta(job(farmer))),
+                            (Choice = 3 -> write('You choose Rancher, lets start ranching'), asserta(job(rancher))).
 
-:- dynamic(isQuest/1).
 quest                   :- isQuest(_) -> write('You have an on-going quest!');
                             \+ isQuest(_), player(X,Y), X =:= 12, Y =:= 8 -> asserta(isQuest(true)), getQuest.
