758886a3b90418a8c84f8b303c926221d74a2e16
ghaziankamil <16520465@std.stei.itb.ac.id>
2021-11-19 21:25:05 +0700

    update marketplace

--- src/marketplace.pl
+++ src/marketplace.pl
@@ -1,17 +1,20 @@
 /*marketplace.pl*/
 
-isMarketplace :- player(X,Y), X =:= 12, Y =:= 12.
+:- dynamic(isMarketplace/1).
 
 market :- \+isStarted(_), write('You have to start your game first!'),!.
-market :- isStarted(_), \+isMarketplace, write('You are not in the marketplace position!'),!. 
-market :- isStarted(_), isMarketplace,
-          write('What do you want to do ?'),nl,
-          write('1. Buy'),nl,
-          write('2. Sell'),!.
+market :- isStarted(_),player(X,Y), X =:= 12, Y =:= 12, (\+ isMarketplace(_), marketOption, asserta(isMarketplace(true));
+          write('You are already in the marketplace !')),!. 
+market :- isStarted(_), \+isMarketplace(_),write('You are not in the marketplace !'),!.
+
+marketOption :- write('Wellcome to marketplace !!!'),nl,
+                write('What do you want to do ?'),nl,
+                write('1. Buy'),nl,
+                write('2. Sell'),!.
 
 buy :- \+isStarted(_), write('You have to start your game first!'),!.
-buy :- isStarted(_), \+isMarketplace, write('You are not in the marketplace position!'),!.
-buy :- isStarted(_), isMarketplace,
+buy :- isStarted(_), \+isMarketplace(_), write('You are not in the marketplace'),!.
+buy :- isStarted(_), isMarketplace(_),
           write('What do you want to buy ?'),nl,
           write('1. Chili seed'),nl,
           write('2. Paddy seed'),nl,
@@ -33,4 +36,17 @@ buy :- isStarted(_), isMarketplace,
           write('18. Level 2 fishing rod'),nl,
           write('19. Level 3 fishing rod'),nl,
           write('20. Milking Equipment'),nl,
-          write('21. Scissors').
+          write('21. Scissors'),!.
+
+sell :- \+isStarted(_), write('You have to start your game first!'),!.
+sell :- isStarted(_), \+isMarketplace(_), write('You are not in the marketplace !'),!.
+sell :- isStarted(_), isMarketplace(_),
+            write('Here are the items in your inventory'),nl,
+            /*Masih dummy dulu*/
+            write('1. ......'),nl, 
+            write('2. ......'),nl,
+            write('3. ......'),nl,
+            write('What do you want to sell ?'),!.
+
+exitMarket :- write('Thanks for coming !!!'),nl,
+            retract(isMarketplace(_)),!.
