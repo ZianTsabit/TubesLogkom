aaaf770d5efa9945a585ea4fc0e820a1244b8525
Vincent136 <13520136@std.stei.itb.ac.id>
2021-11-21 10:19:16 +0700

    delete item

--- src/inventory.pl
+++ src/inventory.pl
@@ -25,7 +25,7 @@ listConsumable :-       findall(Jumlah,inventoryI(_,_,consumable,_,_,Jumlah), Li
 showItem([],[],[]).
 
 showItem([Jumlah|TX],[Level|TY],[Nama|TZ]) :-
-                        write(Jumlah),write(' Level '),write(Level),write(' '),write(Nama),((Jumlah > 1) -> write('s')),nl,
+                        (Jumlah > 0 -> write(Jumlah),write(' Level '),write(Level),write(' '),write(Nama),((Jumlah > 1) -> write('s')),nl),
                         showItem(TX,TY,TZ).
 
 showConsumable([],[]).
@@ -37,38 +37,72 @@ showConsumable([Jumlah|TX],[Nama|TZ]) :-
 kapasitas :-            findall(Jumlah, inventoryI(_,_,_,_,_,Jumlah), ListJumlah),
                         sum(ListJumlah, Kapasitas),
                         retract(capacity(_)),
-                        asserta(capacity(Kapasitas)).
+                        asserta(capacity(Kapasitas)), !.
+
 
 inventory :-            write('Your inventory: '),
                         capacity(Kapasitas),
                         write(Kapasitas), write(' / 100'),nl,
-                        listItem, listConsumable, !.
+                        listItem, listConsumable.
 
-
+/* Logic add item to inventory */
+checkAfter                 :-   capacity(X), X > 100.
 
 addItem(ID,JobItem,Jumlah) :-   item(ID,Nama,JobItem),capacity(X),
-                                X =< 100,
+                                NewX is X + Jumlah, NewX =< 100,
                                 \+inventoryI(ID,_,item,JobItem,_,_), 
-                                asserta(inventoryI(ID,Nama,item,JobItem,1,Jumlah)), kapasitas,!.
+                                asserta(inventoryI(ID,Nama,item,JobItem,1,Jumlah)), kapasitas, !.
 
 addItem(ID,JobItem,Jumlah) :-   item(ID,Nama,JobItem),capacity(X),
-                                X =< 100,
+                                NewX is X + Jumlah, NewX =< 100,
                                 inventoryI(ID,_,item,JobItem,_,JumlahC),
                                 retract(inventoryI(ID,_,item,JobItem,_,_)),
                                 NewJumlah is JumlahC + Jumlah, 
                                 asserta(inventoryI(ID,Nama,item,JobItem,1,NewJumlah)), kapasitas,!.
 
+addItem(ID,JobItem,Jumlah) :-   item(ID,_,JobItem),capacity(X),
+                                NewX is X + Jumlah, NewX > 100,
+                                write('Not enough capacity!').
+                               
+
+
 addConsumable(ID, Jumlah) :-    consumable(ID,Nama),capacity(X),
-                                X =< 100,
+                                NewX is X + Jumlah, NewX =< 100,
                                 \+inventoryI(ID,_,consumable,_,_,_),
                                 asserta(inventoryI(ID,Nama,consumable,non,-1,Jumlah)), kapasitas,!.
 
 addConsumable(ID, Jumlah) :-    consumable(ID,Nama),capacity(X),
-                                X =< 100,
+                                NewX is X + Jumlah, NewX =< 100,
                                 inventoryI(ID,_,consumable,_,_,JumlahC),
                                 retract(inventoryI(ID,_,consumable,_,_,_)),
                                 NewJumlah is JumlahC + Jumlah,
                                 asserta(inventoryI(ID,Nama,consumable,non,-1,NewJumlah)), kapasitas,!.
 
+addConsumable(ID,Jumlah) :-     consumable(ID,_),capacity(X),
+                                NewX is X + Jumlah, NewX > 100,
+                                write('Not enough capacity!').
+
+/* Logic delete item to inventory */
+
+deleteItem(ID,JobItem,Jumlah) :- item(ID,Nama,JobItem),
+                                 inventoryI(ID,Nama,item,JobItem,Level,JumlahI),
+                                 JumlahI >= Jumlah, NewJumlah is JumlahI - Jumlah, 
+                                 retract(inventoryI(ID,Nama,item,JobItem,Level,JumlahI)),
+                                 asserta(inventoryI(ID,Nama,item,JobItem,Level,NewJumlah)), kapasitas, !.
 
 
+deleteItem(ID,JobItem,Jumlah) :- item(ID,Nama,JobItem),
+                                 inventoryI(ID,Nama,item,JobItem,_,JumlahI),
+                                 JumlahI < Jumlah,
+                                 write('invalid amount!'), !.
+
+deleteConsumable(ID, Jumlah) :-  consumable(ID,Nama),
+                                 inventoryI(ID,Nama,consumable,_,_,JumlahC),
+                                 JumlahC >= Jumlah, NewJumlah is JumlahC - Jumlah, 
+                                 retract(inventoryI(ID,Nama,consumable,_,_,JumlahC)),
+                                 asserta(inventoryI(ID,Nama,consumable,non,-1,NewJumlah)), kapasitas, !.
+
+deleteConsumable(ID, Jumlah) :-  consumable(ID,Nama),
+                                 inventoryI(ID,Nama,consumable,_,_,JumlahC),
+                                 JumlahC < Jumlah,
+                                 write('invalid amount!'), !.
