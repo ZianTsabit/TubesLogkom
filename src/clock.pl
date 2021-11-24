6aecf8e169cf8b14f717c70a9e35ecc52f64181a
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-25 02:38:11 +0700

    Update alchemist arrival

--- src/clock.pl
+++ src/clock.pl
@@ -13,8 +13,8 @@ showClock :-        clock(X), Jam is X // 60, Menit is X mod 60,
 clockRules :-       clock(X), X > 1439, 
                     retract(day(Y)),NewY is Y+1,asserta(day(NewY)),
                     retract(clock(X)),NewX is X-1440,asserta(clock(NewX)), 
-                    ((NewY mod 15 =:= 0 -> asserta(alchemist(1,1)), write('Good news!'), nl, write('Alchemist is here'), nl, nl);
-                    (alchemist(A,B), NewY mod 15 =\= 0 -> retract(alchemist(A,B)), write('Alchemist has gone'), nl, nl); !),
+                    ((NewY mod 17 =:= 0 -> asserta(alchemist(1,1)), write('Good news!'), nl, write('Alchemist is here'), nl, nl);
+                    (alchemist(A,B), NewY mod 17 =\= 0 -> retract(alchemist(A,B)), write('Alchemist has gone'), nl, nl); !),
                     (richBoost(_) -> retract(player(C,D,E,F,G,H,I,J,K,Gold)), NewGold is Gold + 40, 
                     asserta(player(C,D,E,F,G,H,I,J,K,NewGold)); !).
 
