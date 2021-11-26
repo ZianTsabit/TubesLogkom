ca92d74a595f2cc183a5b2b1bd70c3125402379b
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-26 16:28:54 +0700

    Add Exp after farm, fish, & ranch

--- src/player.pl
+++ src/player.pl
@@ -2,9 +2,9 @@
 
 /* Code utama player */
 
-initStatus(X)                   :-  asserta(player(X,1,1,0,1,0,1,0,0,1000)).
+initStatus(X)               :-  asserta(player(X,1,1,0,1,0,1,0,0,1000)).
 
-playerStatus                    :-  player(Job,Level,Level_farm,Exp_farm,Level_fish,Exp_fish,Level_ranch,Exp_ranch,Exp,Gold),
+playerStatus                :-  player(Job,Level,Level_farm,Exp_farm,Level_fish,Exp_fish,Level_ranch,Exp_ranch,Exp,Gold),
                                     write('Your status:'), nl,
                                     write('Job: '), write(Job), nl,
                                     write('Level: '), write(Level), nl,
@@ -16,3 +16,160 @@ playerStatus                    :-  player(Job,Level,Level_farm,Exp_farm,Level_f
                                     write('Exp ranching: '), write(Exp_ranch), nl,
                                     write('Exp: '), write(Exp), nl,
                                     write('Gold: '), write(Gold).
+
+expAfter(O,Add_Exp)         :-  retract(player(Job,Level,Level_farm,Exp_farm,Level_fish,Exp_fish,Level_ranch,Exp_ranch,Exp,Gold)),
+                                ((Exp + Add_Exp) >= 100, NewExp is Exp + Add_Exp - 100, NewLevel is Level + 1,
+                                write('Congratulations... You leveled up!'), nl, write('You get 1000 golds'), NewGold is Gold + 1000;
+                                NewExp is Exp + Add_Exp, NewLevel is Level, NewGold is Gold),
+                                (O = farm -> ((Exp_farm + Add_Exp) >= 100, NewExp_farm is Exp_farm + Add_Exp - 100, NewLevel_farm is Level_farm + 1;
+                                NewExp_farm is Exp_farm + Add_Exp, NewLevel_farm is Level_farm),
+                                asserta(player(Job,NewLevel,NewLevel_farm,NewExp_farm,Level_fish,Exp_fish,Level_ranch,Exp_ranch,NewExp,NewGold));
+                                O = fish -> ((Exp_fish + Add_Exp) >= 100, NewExp_fish is Exp_fish + Add_Exp - 100, NewLevel_fish is Level_fish + 1;
+                                NewExp_fish is Exp_fish + Add_Exp, NewLevel_fish is Level_fish),
+                                asserta(player(Job,NewLevel,Level_farm,Exp_farm,NewLevel_fish,NewExp_fish,Level_ranch,Exp_ranch,NewExp,NewGold));
+                                O = ranch -> ((Exp_ranch + Add_Exp) >= 100, NewExp_ranch is Exp_ranch + Add_Exp - 100, NewLevel_ranch is Level_ranch + 1;
+                                NewExp_ranch is Exp_ranch + Add_Exp, NewLevel_ranch is Level_ranch),
+                                asserta(player(Job,NewLevel,Level_farm,Exp_farm,Level_fish,Exp_fish,NewLevel_ranch,NewExp_ranch,NewExp,NewGold))), !.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
